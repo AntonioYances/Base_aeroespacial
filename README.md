e3349c98f3ac97ee7f46d5064a1427a75cfa8841
Joshua Puello <33018356+JoshuaPuello@users.noreply.github.com>
2023-02-20 10:45:47 -0500

    Feedback

--- README.md
+++ README.md
@@ -8,3 +8,20 @@ nombre = "estacion_espacial"
 user="ryffqejwrux749ovhz5l";
 contraseña = "pscale_pw_acMQdWnYI5NMDxpSV6mHX8Ueti4CpRmOjdouV9YdmAH";
 url="jdbc:mysql://us-east.connect.psdb.cloud/" + nombre;
+
+
+--
+
+Temas relevantes a tener en cuenta para estudiar. 
+- Patrón MVC con ejemplos en Java para definir claramente las responsabilidades de cada clase y en qué paquete deben ir.
+- Clean code: Hay una buena lista de tutoriales al respecto: https://www.youtube.com/watch?v=f5mgLVstU1I&list=PL0kIvpOlieSPI2p6VAy8EdCY6dZNgyi8V&ab_channel=gavilanch2
+  - Cada video es útil porque ayuda a tener una idea de cómo formatear el código adecuadamente, estilos, espacios, nombres, funciones, etc, etc.
+- Principios SOLID, super importantes de aquí en adelante, es fundamental poder dominar el tema, además que siempre los preguntan en entrevistas.
+- POO (Programación orientada a objetos). Permite tener una panorama más claro sobre interfaces, clases abstractas, herencia, polimorfismo, etc. 
+- Manejo de exceptiones. 
+- Java SE en general.
+- Empezar a estudiar Spring Framework, es un framework que se usa a nivel empresarial prácticamente en más del 70 % de las empresas, usar Spring Framework en este proyecto ayuda a reducir mucho código porque hay cosas que ya hace internamente por nosotros. 
+  - Spring boot, Spring MVC, Spring Data JPA. 
+- Una vez estos temas estén más consolidados se puede revisar el despliegue de la aplicación a la nube cubriendo otros temas como las credenciales a la base de datos no deben estar expuestas en el código, entre otras cosas. 
+- Importante en el código, son necesarias pruebas unitarias, es fundamental. 
+
--- src/controlador/controladorNaves.java
+++ src/controlador/controladorNaves.java
@@ -11,6 +11,18 @@ import javax.swing.JOptionPane;
 /**
  *
  * @author Antonio Yances
+ * El nombre de la clase no sigue las convenciones de nombres de Java, debería ser ControladorNaves.
+ * La clase implementa la interfaz ActionListener para manejar eventos de botones de la interfaz gráfica, lo que está bien.
+ * Los atributos de la clase no siguen las convenciones de nombres de Java. En general los nombres de las variables deberían ser descriptivos y cortos, por ejemplo, por qué se llama masaS y no masa? Qué significa la S?
+ * Los atributos empujeR y valor son públicos y estáticos, lo que significa que pueden ser modificados desde cualquier parte del programa, lo cual no es recomendable. Además, no se utilizan en ningún momento.
+ * El constructor de la clase recibe tres parámetros y asigna su valor a los atributos correspondientes, lo que está bien.
+ * El método iniciar solo configura algunos atributos de la vista formulario, lo cual está bien.
+ * El método actionPerformed maneja los eventos de los botones de la interfaz gráfica. Sin embargo, es bastante largo y realiza demasiadas tareas. Sería conveniente separar las distintas acciones en métodos más pequeños y fáciles de entender.
+ * Los métodos limpiar y limpiarCampos son prácticamente iguales, solo que uno borra todos los campos y el otro solo algunos. Sería conveniente eliminar uno de ellos y renombrar al otro para que sea más descriptivo.
+ * En general, el código tiene comentarios innecesarios que solo describen lo que hace cada método, lo cual es redundante. Los comentarios deberían explicar por qué se hace algo o aclarar conceptos complejos.
+ * El código no está muy bien organizado y es difícil de seguir. Sería conveniente separar las distintas acciones en métodos más pequeños y fáciles de entender, y renombrar los métodos y atributos con nombres más descriptivos y que sigan las convenciones de nombres de Java.
+ * Faltan validaciones en algunos de los campos de la vista, como la velocidad y la masa, para asegurarse de que se ingresen valores válidos.
+ * No se comprende bien la diferencia de responsabilidades entre controladorNaves/Empuje/Mision/Tripulacion, tal vez agruparlos en paquetes más representativos puede ayudar a comprender eso. También el nombre de las clases ayuda a captar la idea específica del proyecto. Mision/Empuje/Tripulación se podrían asociar con nombres de modelos/entidades en lugar de lógica de negocio.
  */
 public class controladorNaves implements ActionListener {
     private String empuje,masaS,aceleracionS;
